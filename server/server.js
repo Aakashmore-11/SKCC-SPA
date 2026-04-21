2d76138794de5b326e9bd53440a2cee8309927c3
Aakash More <gamingwithsky48@gmail.com>
2026-04-21 16:48:02 +0530

    Hii

--- server/server.js
+++ server/server.js
@@ -172,8 +172,8 @@ app.get('/api/auth/verify', (req, res) => {
 
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'dist', 'index.html')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
